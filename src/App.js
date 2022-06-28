7f1edcfb60b41782fd7490749f59ac6f8bf19e5b
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-06-28 15:29:06 +0300

    Add lazy component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import { React, lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Team from './components/Team';
-import Project from './components/Projects';
-import Contact from './components/Contact';
 import './App.css';
+
+const Home = lazy(() => import('./components/Home'));
+const Team = lazy(() => import('./components/Team'));
+const Project = lazy(() => import('./components/Projects'));
+const Contact = lazy(() => import('./components/Contact'));
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="team" element={<Team />} />
-        <Route path="project" element={<Project />} />
-        <Route path="about" element={<Contact />} />
-      </Routes>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="team" element={<Team />} />
+          <Route path="project" element={<Project />} />
+          <Route path="about" element={<Contact />} />
+        </Routes>
+      </div>
+    </Suspense>
   );
 }
 
